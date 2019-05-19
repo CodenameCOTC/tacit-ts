e72e10fdaded027d8eb177ada0da2d436b8a367c
A. Noval Adip <a.novaladip@yahoo.com>
2019-05-20 00:17:29 +0700

    add colors

---
+++ src/constants/colors/index.ts
@@ -0,0 +1,6 @@
+export const colors = {
+    primary: '#353941',
+    primaryStatusBar: '#292c33',
+    primaryLight: '#FEFCF9',
+    primaryDark: '#171a1e',
+};
--- src/navigation/index.ts
+++ src/navigation/index.ts
@@ -1,6 +1,7 @@
-import { LayoutStack, LayoutStackChildren, Navigation } from 'react-native-navigation';
+import { LayoutStack, Navigation } from 'react-native-navigation';
 
 import { HomePage } from './home';
+import { colors } from '../constants';
 
 export const defaultHomeLayoutStack: LayoutStack = {
     children: [HomePage],
@@ -9,13 +10,13 @@ export const defaultHomeLayoutStack: LayoutStack = {
 export const App = (layoutStack: LayoutStack = defaultHomeLayoutStack) => {
     Navigation.setDefaultOptions({
         statusBar: {
-            backgroundColor: 'white',
+            backgroundColor: colors.primaryStatusBar,
             style: 'light',
         },
         topBar: {
-            background: { color: 'red' },
-            backButton: { color: 'white' },
-            title: { color: 'white', alignment: 'center' },
+            background: { color: colors.primary },
+            backButton: { color: colors.primaryLight },
+            title: { color: colors.primaryLight, alignment: 'center' },
         },
     });
 
